e3d2ed60b10632dd42bf9db45ae452b73efcbcf6
prince kumar <sprince05873@gmail.com>
2025-07-19 21:16:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// script.js
+    document.addEventListener('DOMContentLoaded', () => {
+      const sections = document.querySelectorAll('section');
+
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+          }
+        });
+      }, {
+        threshold: 0.1
+      });
+
+      sections.forEach(section => {
+        section.classList.add('hidden');
+        observer.observe(section);
+      });
+    });
